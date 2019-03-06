41f13da06429386db71cbdb81df77aaaf921fefb
Charley McGowan and Phil Mass <charley+phil@>
2019-03-06 09:07:28 -0800

    add form to app

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,42 @@
 import React from 'react';
 import styles from './App.css';
 
-function App() {
-  return (
-    <div className={styles.app}>
-      <h1>React Application</h1>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    event.preventDefault();
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log('submit', this.state);
+  }
+
+  render() {
+    return (
+      <div className={styles.app}>
+        <h1>React Application</h1>
+        <form onSubmit={this.handleSubmit}>
+          <p>Your Name</p>
+          <input id='heroName' name='name' type='text' onChange={this.handleChange} />
+          <p>Your Powers</p>
+          <textarea id='heroPowers' name='powers' type='text' onChange={this.handleChange} />
+          <div>
+            <button type='submit'>Submit</button>
+          </div>
+        </form>
+      </div> 
+    );
+  }
 }
 
 export default App;
